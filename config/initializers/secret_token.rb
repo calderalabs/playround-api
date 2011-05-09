ec1fa7449bb32ba8b797b84f37196331ee6c4898
Eugenio Depalo <eugeniodepalo@gmail.com>
2011-05-09 20:11:36 +0200

    Created stub project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playround::Application.config.secret_token = '9a534997d31c538365ebc963eae183acb285fe6bee31fd657343e95cd40a28cfa022a66e011593f1f0ec71a4a2ecc6ef6a198295ba3d99a861c862861adc6061'
