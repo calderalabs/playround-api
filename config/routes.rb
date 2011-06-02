3b4aae1059d124f784f4bccadaf84bf7242309c8
Eugenio Depalo <eugeniodepalo@gmail.com>
2011-06-02 18:52:50 +0200

    Improved user interface

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Playround::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "rounds#index"
+  root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
