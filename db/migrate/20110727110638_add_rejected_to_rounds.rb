d5bd7aa105d9338ed97360293d6860d17599f2ff
Matteo Depalo <matteodepalo@gmail.com>
2011-07-29 15:48:23 +0200

    Removed rejected attribute from rounds

--- db/migrate/20110727110638_add_rejected_to_rounds.rb
+++
@@ -1,9 +0,0 @@
-class AddRejectedToRounds < ActiveRecord::Migration
-  def self.up
-    add_column :rounds, :rejected, :boolean, :default => false
-  end
-
-  def self.down
-    remove_column :rounds, :rejected
-  end
-end
