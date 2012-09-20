19ce82db807eaf17674107cc80bdf2be8b839cdc
Matteo Depalo <matteodepalo@gmail.com>
2012-09-20 13:58:20 +0200

    Refactored assets placement for asset pipeline compatbility

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,15 @@ Playround::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
   
   PAPERCLIP_CONFIG = {
     :storage => :s3,
